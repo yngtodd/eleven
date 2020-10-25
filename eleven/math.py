a3e4446aa6c929937c7cb3780d0661575266baa5
yngtodd <young.todd.mk@gmail.com>
2020-10-25 19:06:48 -0400

    Add a math module
    This will give us some simple functions to try our unit testing.

---
+++ eleven/math.py
@@ -0,0 +1,12 @@
+def add_one(x: int):
+    """Add one to `x`
+
+    Args:
+        x: an integer
+    """
+    return x + 1
+
+
+def halve(x: float):
+    """Divide a number by 2"""
+    retrun x / 2
