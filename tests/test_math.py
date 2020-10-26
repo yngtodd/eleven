5f23786435166d1706d339d7837e4ee6b00449b7
yngtodd <young.todd.mk@gmail.com>
2020-10-26 17:40:45 -0400

    Add simple fixture
    This just shows how we can setup some state for our tests.

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,19 +1,26 @@
 """Test the functions in our math.py module"""
+import pytest
 
 from eleven import math
+
+
+@pytest.fixture
+def test_value():
+   val = 2
+   return val
 
 
 def test_add_negative():
     assert math.add_one(-1) == 0
 
 
-def test_add_positive():
-    assert math.add_one(1) == 2
+def test_add_positive(test_value):
+    assert math.add_one(1) == test_value
 
 
 def test_halve():
     assert math.halve(2.0) == 1.0
 
 
-def test_halve_int():
-    assert math.halve(4) == 2
+def test_halve_int(test_value):
+    assert math.halve(4) == test_value
